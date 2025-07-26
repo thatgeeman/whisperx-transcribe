4b12a16e5c21244adc5a3ecc8255bff78e1b1ab4
Geevarghese George <Geevarghese.George@avl.com>
2025-07-26 12:12:08 +0200

    fix cudnn version

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
+# Notes
+
 Cluster backend do not support efficient float16 computation.
 
-Install `ffmpeg`, `rust`, `cudnn` from conda.
+- Quadro P4000
 
-Future me: `conda install cudnn` installed:
+## Alternatives
+
+Replicate provides inference. See [colab](https://colab.research.google.com/drive/1FH50NOULkMUawgvXR7H9gSc4LpDaM5H8).
+
+## Environment Setup
+
+Python 3.10 used.
+
+Install `ffmpeg`, `rust`, `cudnn=8.9.7` from conda.
+
+Future me: `conda install cudnn=8.9.7` installed:
 
 ```
 cuda-nvrtc-12.9.86
 cuda-version-12.9
-cudnn-9.10.1.4
+cudnn-8.9.7.29
 libcublas-12.9.1.4
-libcudnn-9.10.1.4
-libcudnn-dev-9.10.1.4
+libcudnn-8.9.7.29
+libcudnn-dev-8.9.7.29
 ```
 
 See `pyproject.toml` for python dependencies.
+
+## HF
+
+Accept terms for
+
+- https://huggingface.co/pyannote/speaker-diarization-3.1
+- https://huggingface.co/pyannote/segmentation-3.0
--- src/whisperx_transcribe/infer.py
+++ src/whisperx_transcribe/infer.py
@@ -1,17 +1,32 @@
+import datetime
 import gc
 import json
+import logging
 
 import torch
 import utils as ut
 import whisperx
 
+logger = logging.getLogger(__name__)
+
 device = "cuda"
 audio_file = "assets/sample/audio.mp3"
 batch_size = 8  # reduce if low on GPU mem
 compute_type = "int8"  # change to "int8" if low on GPU mem (may reduce accuracy)
+start_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+audio_path = audio_file.split(".")[0]
+audio_stem = audio_path.split("/")[-1]
 
-# 1. Transcribe with original whisper (batched)
-model = whisperx.load_model("large-v2", device, compute_type=compute_type)
+format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+file_handler = logging.FileHandler(f"logs/{audio_stem}_{start_time}.log", mode="w")
+file_handler.setFormatter(logging.Formatter(format))
+logger.addHandler(file_handler)
+
+logger.setLevel(logging.INFO)
+
+logger.info(
+    f"Transcribing {audio_file} with batch size {batch_size} and compute type {compute_type}"
+)
 
 # save model to local path (optional)
 model_dir = "model/"
@@ -21,7 +36,7 @@ model = whisperx.load_model(
 
 audio = whisperx.load_audio(audio_file)
 result = model.transcribe(audio, batch_size=batch_size)
-print(result["segments"])  # before alignment
+logger.info("Transcription complete")
 
 # delete model if low on GPU resources
 gc.collect()
@@ -36,10 +51,13 @@ result = whisperx.align(
     result["segments"], model_a, metadata, audio, device, return_char_alignments=False
 )
 
-print(result["segments"])  # after alignment
+logger.info("Transcription alignment complete")  # after alignment
 
 # delete model if low on GPU resources
-# import gc; import torch; gc.collect(); torch.cuda.empty_cache(); del model_a
+
+gc.collect()
+torch.cuda.empty_cache()
+del model_a
 
 # 3. Assign speaker labels
 diarize_model = whisperx.diarize.DiarizationPipeline(
@@ -49,12 +67,20 @@ diarize_model = whisperx.diarize.DiarizationPipeline(
 # add min/max number of speakers if known
 diarize_segments = diarize_model(audio)
 # diarize_model(audio, min_speakers=min_speakers, max_speakers=max_speakers)
+logger.info("Speaker diarization complete")
 
 result = whisperx.assign_word_speakers(diarize_segments, result)
 
 # write json to file
-with open(f"{audio_file.split('.')[0]}_transcript.json") as f:
+with open(f"{audio_path}_diarized.json", "w") as f:
     json.dump(result, f, indent=4)
 
-with open(f"{audio_file.split('.')[0]}_segmented.json", "w") as f:
-    json.dump(diarize_segments, f, indent=4)
+logger.info(f"Diarized results saved to {audio_path}_diarized.json")
+
+# 4. Write segments to file
+with open(f"{audio_path}_segments.txt", "w") as f:
+    for segment in result["segments"]:
+        f.write(f"{segment['start']:.2f} --> {segment['end']:.2f}\n")
+        f.write(f"{segment['text']}\n\n")
+
+logger.info(f"Segments saved to {audio_path}_segments.txt")
